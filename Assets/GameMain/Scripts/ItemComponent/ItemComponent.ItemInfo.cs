28eed2345fa90fc042303f0e9b3e052c0074b717
DrFlower <303333530@qq.com>
2020-05-18 22:36:41 +0800

    编写ItemComponent

---
+++ Assets/GameMain/Scripts/ItemComponent/ItemComponent.ItemInfo.cs
@@ -0,0 +1,18 @@
+﻿using GameFramework.ObjectPool;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Flower.ItemComponent
+{
+    public sealed partial class ItemComponent : GameFrameworkComponent
+    {
+        public class ItemInfo
+        {
+
+        }
+    }
+ 
+}
+
+
---
+++ Assets/GameMain/Scripts/ItemComponent/ItemComponent.cs
@@ -0,0 +1,26 @@
+﻿using GameFramework.ObjectPool;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Flower.ItemComponent
+{
+    public sealed partial class ItemComponent : GameFrameworkComponent
+    {
+
+
+
+        private void Start()
+        {
+
+        }
+
+
+        private void Update()
+        {
+
+        }
+    }
+}
+
+
