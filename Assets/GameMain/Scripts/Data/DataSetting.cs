229d253475f96d38ca3e292c371d0d0d544f24ca
DrFlower <303333530@qq.com>
2020-05-30 00:55:45 +0800

    DataSetting

---
+++ Assets/GameMain/Scripts/Data/DataSetting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework.Data;
+
+namespace Flower
+{
+    public class DataSetting : DataBase
+    {
+        public override void Init()
+        {
+            Debug.LogError("DataSetting Init");
+        }
+
+        public override void OnPreload()
+        {
+            Debug.LogError("DataSetting OnPreload");
+        }
+
+        public override void OnLoad()
+        {
+            Debug.LogError("DataSetting OnLoad");
+        }
+
+        public override void OnUnload()
+        {
+            Debug.LogError("DataSetting OnUnload");
+        }
+
+        public override void Shutdown()
+        {
+            Debug.LogError("DataSetting Shutdown");
+        }
+    }
+
+}
