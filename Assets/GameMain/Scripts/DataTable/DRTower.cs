74f5e831dc419031748a1d1f625c4fd042a9e1f0
DrFlower <303333530@qq.com>
2020-06-11 23:57:00 +0800

    添加Tower,TowerLevel表

--- Assets/GameMain/Scripts/DataTable/DRTower.cs
+++ Assets/GameMain/Scripts/DataTable/DRTower.cs
@@ -5,7 +5,7 @@
 // Feedback: mailto:ellan@gameframework.cn
 //------------------------------------------------------------
 // 此文件由工具自动生成，请勿直接修改。
-// 生成时间：2020-06-11 23:54:53.901
+// 生成时间：2020-06-11 23:54:53.921
 //------------------------------------------------------------
 
 using GameFramework;
@@ -19,14 +19,14 @@ using UnityGameFramework.Runtime;
 namespace Flower
 {
     /// <summary>
-    /// 声音资源配置表。
+    /// 炮塔配置表。
     /// </summary>
-    public class DRSound : DataRowBase
+    public class DRTower : DataRowBase
     {
         private int m_Id = 0;
 
         /// <summary>
-        /// 获取声音编号。
+        /// 获取配置编号。
         /// </summary>
         public override int Id
         {
@@ -37,7 +37,16 @@ namespace Flower
         }
 
         /// <summary>
-        /// 获取资源ID名称。
+        /// 获取炮塔名字Id。
+        /// </summary>
+        public string NameId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取资源ID。
         /// </summary>
         public int AssetId
         {
@@ -46,18 +55,9 @@ namespace Flower
         }
 
         /// <summary>
-        /// 获取声音组名称。
+        /// 获取图标名称。
         /// </summary>
-        public int SoundGroupId
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// 获取声音播放参数。
-        /// </summary>
-        public int SoundPlayParamId
+        public string Icon
         {
             get;
             private set;
@@ -78,9 +78,9 @@ namespace Flower
                 index++;
                 m_Id = int.Parse(columnTexts[index++]);
                 index++;
+                NameId = columnTexts[index++];
                 AssetId = int.Parse(columnTexts[index++]);
-                SoundGroupId = int.Parse(columnTexts[index++]);
-                SoundPlayParamId = int.Parse(columnTexts[index++]);
+                Icon = columnTexts[index++];
             }
             else if (dataType == typeof(byte[]))
             {
@@ -90,9 +90,9 @@ namespace Flower
                     using (BinaryReader binaryReader = new BinaryReader(memoryStream, Encoding.UTF8))
                     {
                         m_Id = binaryReader.Read7BitEncodedInt32();
+                        NameId = strings[binaryReader.Read7BitEncodedInt32()];
                         AssetId = binaryReader.Read7BitEncodedInt32();
-                        SoundGroupId = binaryReader.Read7BitEncodedInt32();
-                        SoundPlayParamId = binaryReader.Read7BitEncodedInt32();
+                        Icon = strings[binaryReader.Read7BitEncodedInt32()];
                     }
                 }
             }
--- Assets/GameMain/Scripts/DataTable/DRTowerLevel.cs
+++ Assets/GameMain/Scripts/DataTable/DRTowerLevel.cs
@@ -5,7 +5,7 @@
 // Feedback: mailto:ellan@gameframework.cn
 //------------------------------------------------------------
 // 此文件由工具自动生成，请勿直接修改。
-// 生成时间：2020-06-11 23:54:53.901
+// 生成时间：2020-06-11 23:54:53.924
 //------------------------------------------------------------
 
 using GameFramework;
@@ -19,14 +19,14 @@ using UnityGameFramework.Runtime;
 namespace Flower
 {
     /// <summary>
-    /// 声音资源配置表。
+    /// 炮塔等级配置表。
     /// </summary>
-    public class DRSound : DataRowBase
+    public class DRTowerLevel : DataRowBase
     {
         private int m_Id = 0;
 
         /// <summary>
-        /// 获取声音编号。
+        /// 获取配置编号。
         /// </summary>
         public override int Id
         {
@@ -37,27 +37,54 @@ namespace Flower
         }
 
         /// <summary>
-        /// 获取资源ID名称。
+        /// 获取炮塔描述Id。
         /// </summary>
-        public int AssetId
+        public string DesId
         {
             get;
             private set;
         }
 
         /// <summary>
-        /// 获取声音组名称。
+        /// 获取模型资源ID。
         /// </summary>
-        public int SoundGroupId
+        public int ModelAssetId
         {
             get;
             private set;
         }
 
         /// <summary>
-        /// 获取声音播放参数。
+        /// 获取DPS。
         /// </summary>
-        public int SoundPlayParamId
+        public float DPS
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取范围。
+        /// </summary>
+        public float Range
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取建造能量。
+        /// </summary>
+        public int BuildEnergy
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取出售能量。
+        /// </summary>
+        public int SellEnergy
         {
             get;
             private set;
@@ -78,9 +105,12 @@ namespace Flower
                 index++;
                 m_Id = int.Parse(columnTexts[index++]);
                 index++;
-                AssetId = int.Parse(columnTexts[index++]);
-                SoundGroupId = int.Parse(columnTexts[index++]);
-                SoundPlayParamId = int.Parse(columnTexts[index++]);
+                DesId = columnTexts[index++];
+                ModelAssetId = int.Parse(columnTexts[index++]);
+                DPS = float.Parse(columnTexts[index++]);
+                Range = float.Parse(columnTexts[index++]);
+                BuildEnergy = int.Parse(columnTexts[index++]);
+                SellEnergy = int.Parse(columnTexts[index++]);
             }
             else if (dataType == typeof(byte[]))
             {
@@ -90,9 +120,12 @@ namespace Flower
                     using (BinaryReader binaryReader = new BinaryReader(memoryStream, Encoding.UTF8))
                     {
                         m_Id = binaryReader.Read7BitEncodedInt32();
-                        AssetId = binaryReader.Read7BitEncodedInt32();
-                        SoundGroupId = binaryReader.Read7BitEncodedInt32();
-                        SoundPlayParamId = binaryReader.Read7BitEncodedInt32();
+                        DesId = strings[binaryReader.Read7BitEncodedInt32()];
+                        ModelAssetId = binaryReader.Read7BitEncodedInt32();
+                        DPS = binaryReader.ReadSingle();
+                        Range = binaryReader.ReadSingle();
+                        BuildEnergy = binaryReader.Read7BitEncodedInt32();
+                        SellEnergy = binaryReader.Read7BitEncodedInt32();
                     }
                 }
             }
