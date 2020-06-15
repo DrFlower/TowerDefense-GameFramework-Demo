78633f2ab6c5566da57a52e8098b182c959d3505
DrFlower <303333530@qq.com>
2020-06-16 00:30:34 +0800

    添加DataPlayer类

---
+++ Assets/GameMain/Scripts/Data/Player/DataPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework.Data;
+using GameFramework.DataTable;
+
+namespace Flower
+{
+    public sealed class DataPlayer : DataBase
+    {
+        public int HP { get; private set; }
+        public int Energy { get; private set; }
+
+        protected override void OnInit()
+        {
+        }
+
+        protected override void OnPreload()
+        {
+        }
+
+        protected override void OnLoad()
+        {
+        }
+
+        public void Damage(int value)
+        {
+
+        }
+
+        public void AddEnergy(int value)
+        {
+
+        }
+
+        public void BuyTower()
+        {
+
+        }
+
+        public void SellTower()
+        {
+
+        }
+
+        protected override void OnUnload()
+        {
+        }
+
+        protected override void OnShutdown()
+        {
+        }
+    }
+}
+
+
