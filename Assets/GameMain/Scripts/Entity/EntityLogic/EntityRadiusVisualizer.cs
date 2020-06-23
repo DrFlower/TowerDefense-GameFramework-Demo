697254723aa0e554994861d0ff4fbb3fd2d11258
DrFlower <303333530@qq.com>
2020-06-23 21:33:21 +0800

    commit

--- Assets/GameMain/Scripts/Entity/EntityLogic/EntityRadiusVisualizer.cs
+++ Assets/GameMain/Scripts/Entity/EntityLogic/EntityRadiusVisualizer.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityGameFramework.Runtime;
 
 namespace Flower
 {
     public class EntityRadiusVisualizer : EntityLogicEx
     {
-
+        public Transform radiusVisualizer;
         public float radiusVisualizerHeight = 0.02f;
 
         public Vector3 localEuler;
 
+        private EntityDataRadiusVisualiser entityDataRadiusVisualiser;
 
         protected override void OnShow(object userData)
         {
             base.OnShow(userData);
+
+            radiusVisualizer.localEulerAngles = localEuler;
+
+            entityDataRadiusVisualiser = userData as EntityDataRadiusVisualiser;
+            if (entityDataRadiusVisualiser == null)
+            {
+                Log.Error("EntityDataRadiusVisualiser data is invalid.");
+                return;
+            }
+
+            radiusVisualizer.localScale = new Vector3(entityDataRadiusVisualiser.Radius, 0, 0);
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
@@ -25,6 +38,7 @@ namespace Flower
         protected override void OnHide(bool isShutdown, object userData)
         {
             base.OnHide(isShutdown, userData);
+            entityDataRadiusVisualiser = null;
         }
 
     }
